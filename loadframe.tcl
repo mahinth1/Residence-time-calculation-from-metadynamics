343832ae5a66ed2ce8cfbff3af989ab82f18df07
Paween Mahinthichaichan <113048114+mahinth1@users.noreply.github.com>
2024-08-17 09:58:29 -0400

    Update loadframe.tcl

--- loadframe.tcl
+++ loadframe.tcl
@@ -1,21 +1,24 @@
 ###Generate new dcd files for PLUMED analysis ####
 ###20 simulation runs ###
-for {set f 0 } {$f < 20} {incr f } { 
+for {set f 0 } {$f < 20} {incr f } {
 
 set in [open exittime.dat r]
 set dat [ split [read $in ] "\n"]
-set lastframe [lindex $dat $f 2] 
+set lastframe [lindex $dat $f 2]
 
-    mol load psf /scratch/Liu_Group/paween/hsp90/hsp90/1yet_unsat/prep_17aag/boundnowater.psf 
+    #input psf file
+    mol load psf boundnowater.psf
+    #input trajectory file
     mol addfile ./run${f}/traj.dcd last ${lastframe} waitfor all
     package require pbctools
     pbc unwrap -sel "protein"
     pbc wrap  -compound residue -sel "not protein" -center com -centersel "protein" -all
     set n [molinfo top get numframes]
+    #protein residues as references for alignment
     for {set i 0} {$i < $n} {incr i} {
     set ref [atomselect top "protein and name CA and resid 20 to 94 146 to 220" frame 0]
     set sel [atomselect top "protein and name CA and resid 20 to 94 146 to 220" frame $i]
-    set all [atomselect top "all" frame $i ] 
+    set all [atomselect top "all" frame $i ]
     $all move [measure fit $sel $ref]
 
     source sasalig.tcl
